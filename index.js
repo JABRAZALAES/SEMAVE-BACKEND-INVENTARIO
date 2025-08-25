f0ee4a9536c49fbccc30bae27ba8aa4e3c439252
Josue Andres Brazales Cango <112193488+JABRAZALAES@users.noreply.github.com>
2025-08-25 13:06:15 -0500

    Actualiza index.js y otros cambios

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+// server.js
 const express = require('express');
 const sequelize = require('./db');
 const Producto = require('./models/Producto');
 const Movimiento = require('./models/Movimiento');
 const productoRoutes = require('./routes/productoRoutes');
 const movimientoRoutes = require('./routes/movimientoRoutes');
-const app = express();
 const cors = require('cors');
+
+const app = express();
 app.use(express.json());
 
 // Activar CORS para todas las solicitudes
@@ -13,19 +15,24 @@ app.use(cors());
 
 // Rutas de productos
 app.use('/api/productos', productoRoutes);
+
 // Rutas de movimientos
 app.use('/api/movimientos', movimientoRoutes);
-// Sincronizar DB
-sequelize.sync({ alter: true })
-  .then(() => console.log('✅ Base de datos sincronizada con MySQL'))
-  .catch(err => console.error('❌ Error al conectar DB:', err));
 
 // Ruta de prueba
 app.get('/', (req, res) => {
   res.send('API Inventario funcionando 🚀');
 });
 
-sequelize.sync().then(() => {
-  console.log('✅ DB sincronizada');
-  app.listen(3000, '0.0.0.0', () => console.log('Servidor corriendo en http://0.0.0.0:3000'));
-});
+// Puerto asignado por Railway o 3000 por defecto
+const PORT = process.env.PORT || 3000;
+
+// Conectar a la DB y levantar servidor
+sequelize.authenticate()
+  .then(() => {
+    console.log('✅ Conectado correctamente a Railway');
+    app.listen(PORT, '0.0.0.0', () => {
+      console.log(`Servidor corriendo en http://0.0.0.0:${PORT}`);
+    });
+  })
+  .catch(err => console.error('❌ Error al conectar DB:', err));
